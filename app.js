068000ca5ce139e93379fc283497803ecf6c0045
Hasnainqari <100525567+Hasnainqari@users.noreply.github.com>
2022-06-02 13:23:59 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// Chapter No 17 to 20;
+// Question No 01;
+// var counting = [1,2,3,4,5,6,7,8,9,10];
+// for(var i = 0; i < counting.length; i++){
+//     document.write(counting[i] + "<br/>");
+// }
+
+
+// Question No 02;
+// var table = +prompt("Enter No Of Table");
+// for(var i = 1; i <= 10; i++){
+//     document.write(table + " x " + i + " = " + table*i + "<br/>");
+// };
+
+
+// Question No 03;
+// var fruits =["Apple","Banana","Mango","Orange","Strawberry","Amrood","Peach","Lime","Grapes","Peer","Cherry","Coconutt","Pineapple"];
+// for(var i = 0; i < fruits.length; i++){
+//     document.write(fruits[i] + "<br/>");
+// }
+
+
+// Question No 04;
+var bakery = ["Apple","Banana","Cake","Pastry","Pineapple","Grapes"];
+
+var bak = prompt("Enter Item name with First letter must be Capital");
+
+var match = "NO";
+
+for(var i = 0; i < bakery.length; i++){
+    if(bakery[i] === bak){
+        match = "YES"
+        alert ("Congratulation " + bakery[i]+" is Availabe");
+        break
+    }
+}
+
+if(match === "NO"){
+    alert("Sorry!!! This Item is not found")
+};
